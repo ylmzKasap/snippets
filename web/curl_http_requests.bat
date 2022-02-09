94e8ddeb0c7c1890bda8ba95818116e73dee7a12
Yılmaz Kasap <ylmzkasap@outlook.com>
2022-02-09 18:53:55 +0300

    Added web snippets.

---
+++ web/curl_http_requests.bat
@@ -0,0 +1,18 @@
+&:: Get request
+
+curl -X GET "http://192.168.1.34:3000/u/hayri" -m 30 -v em
+
+
+&:: Post Request with Body
+
+curl -X POST http://192.168.1.34:3000/u/hayri/create_folder -H "Content-Type: application/json" -d "{\"folder_name\": \"haha yes\", \"parent_id\": 1}"
+
+
+&:: Put Request with Body
+
+curl -X PUT http://192.168.1.34:3000/updatedir/hayri -H "Content-Type: application/json" -d "{\"item_id\": 8, \"parent_id\": 3, \"item_name\": \"file_1\", \"direction\": \"parent\"}"
+
+
+&:: Delete Request With Body
+
+curl -X DELETE http://192.168.1.34:3000/u/hayri/delete_item -H "Content-Type: application/json" -d "{\"item_id\": 43, \"parent_id\": 1}"
